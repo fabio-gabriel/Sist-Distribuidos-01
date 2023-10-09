2229d8ad9705c8945fa71ed08b6764215c41716a
fabio-gabriel <fabiogabrieleig@gmail.com>
2023-10-08 23:26:26 -0300

    fixes and comment updates

--- src/gateway.py
+++ src/gateway.py
@@ -2,7 +2,7 @@ import socket
 import threading
 import sys
 import os
-from table import print_table
+from table import print_table  # Assuming you have a custom "print_table" function
 import proto.device_pb2
 
 
@@ -11,16 +11,17 @@ class IoTGateway:
         self.host = host
         self.port = port
         self.clients = {}
-        self.device_id = 0  # counter for the device ids
-        self.running = True
+        self.device_id = 0  # Counter for the device IDs
+        self.running = True  # Flag to control main loop
 
     def start(self):
+        # Create and configure the server socket
         server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         server_socket.bind((self.host, self.port))
         server_socket.listen(5)
         print(f"Gateway is listening on {self.host}:{self.port}")
 
-        # Daemon thread for picking up user commands
+        # Daemon thread for handling user commands
         user_command_thread = threading.Thread(target=self.handle_user_commands)
         user_command_thread.daemon = True  # Set as a daemon thread to exit when the main thread exits
         user_command_thread.start()
@@ -33,12 +34,14 @@ class IoTGateway:
             client_thread.start()
 
     def handle_client(self, client_socket):
+        # Receive and decode the device type sent by the client
         try:
             device_type = client_socket.recv(1024).decode()
 
             device_id = self.device_id
             self.device_id += 1  # Increment the device ID counter
 
+            # Store client information in a dictionary
             self.clients[device_id] = {"socket": client_socket, "type": device_type}
             print(f"Assigned Device ID {device_id} to client {client_socket.getpeername()} of type {device_type}")
 
@@ -48,12 +51,14 @@ class IoTGateway:
                     break
                 self.handle_messages(data, device_type)
 
-            self.clients.remove(client_socket)
-            client_socket["socket"].close()
+            # Remove the client from the dictionary and close the socket
+            del self.clients[device_id]
+            client_socket.close()
         except Exception as e:
             print(f"Error handling client: {e}")
 
     def handle_messages(self, data, device_type):
+        # Handle messages based on the device type
         if device_type == "thermostat":
             message = proto.device_pb2.ThermostatStatus()
             message.ParseFromString(data)
@@ -62,11 +67,12 @@ class IoTGateway:
 
         elif device_type == "airconditioner":
             message = proto.device_pb2.StatusMessage()
-            message.ParseFromString(message)
+            message.ParseFromString(data)
 
             print(message)
 
     def send_message(self, device_id):
+        # Find the dictionary entry with the matching device ID
         for device_id in self.clients:
             device_type = self.clients[device_id]["type"]
             device_socket = self.clients[device_id]["socket"]
@@ -101,7 +107,7 @@ class IoTGateway:
             except Exception as e:
                 print(f"Error sending status message: {e}")
 
-    # Broadcast function
+    # Broadcast function to send data to all devices
     def broadcast_data(self):
         for device_id, device_info in self.clients.items():
             client_socket = device_info["socket"]
@@ -120,6 +126,7 @@ class IoTGateway:
 
                     wrapper = proto.device_pb2.LightbulbMessage()
                     wrapper.status.CopyFrom(message)
+
                 client_socket.send(wrapper.SerializeToString())
 
             except Exception as e:
@@ -130,15 +137,16 @@ class IoTGateway:
             user_input = input("Enter a command (or 'quit' to exit): ")
             if user_input.lower() == "quit":
                 print("Closing the gateway...")
-                gateway.running = False
+                self.running = False  # Set the running flag to False
                 for device_id, device_info in self.clients.items():
                     device_info["socket"].close()
-                os._exit(1)
+                os._exit(1)  # Exit the program
 
             elif user_input.lower() == "status":
                 self.broadcast_data()
 
             elif user_input.lower() == "help":
+                # Display available commands and descriptions
                 commands = {
                     "\nstatus": "Check the current status of the program.",
                     "quit": "Exit the program.",
@@ -149,10 +157,12 @@ class IoTGateway:
                     print(f"{command}: {desc}")
 
             elif user_input.lower() == "update":
-                device = input("Input the device id: ")
+                # Send a message to a specific device
+                device = input("Input the device ID: ")
                 self.send_message(device)
 
             elif user_input.lower() == "show":
+                # Show the list of connected clients
                 print(self.clients)
 
             else:
@@ -161,4 +171,4 @@ class IoTGateway:
 
 if __name__ == "__main__":
     gateway = IoTGateway("localhost", 8080)
-    gateway.start()
+    gateway.start()  # Start the IoT gateway
